9ab344d0d9dfb7a8e04772c75623185d4cad42b4
Lance Zotigh <lancezotighlz@gmail.com>
2019-03-06 14:54:25 -0700

    Spinners Working!...Kinda

--- app/src/main/java/edu/cnm/deepdive/foodrandom/fragments/RandomizeFragment.java
+++ app/src/main/java/edu/cnm/deepdive/foodrandom/fragments/RandomizeFragment.java
@@ -7,26 +7,47 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.Spinner;
 import edu.cnm.deepdive.foodrandom.R;
 
 public class RandomizeFragment extends Fragment{
+
+  Spinner spinnerVegitable;
+  Spinner spinnerMeat;
+  Button ranButton;
+
 
   @Nullable
   @Override
   public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
       @Nullable Bundle savedInstanceState) {
     View view = inflater.inflate(R.layout.fragment_randomize, container, false);
-//
-//    Spinner spinner = findViewById(R.id.spinnerMeat);
-//    ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.numbers, android.R.layout.simple_spinner_item);
-//    adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-//    spinner.setAdapter(adapter);
-//    spinner.setOnItemSelectedListener(this);
-     return view;
 
+    spinnerVegitable = view.findViewById(R.id.spinnerVegetable);
+    spinnerMeat =  view.findViewById(R.id.spinnerMeat);
+
+    ArrayAdapter<CharSequence> adapter1 = ArrayAdapter.createFromResource(getActivity(),
+        R.array.meat_selection, android.R.layout.simple_spinner_item);
+    adapter1.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+    spinnerMeat.setAdapter(adapter1);
+
+    ArrayAdapter<CharSequence> adapter2 = ArrayAdapter.createFromResource(getActivity(),
+        R.array.vegetables_selection, android.R.layout.simple_spinner_item);
+    adapter2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+    spinnerVegitable.setAdapter(adapter2);
+
+//    ranButton.setOnClickListener(new OnClickListener() {
+//      @Override
+//      public void onClick(View view) {
+//        spinnerMeat.getSelectedItem().toString();
+//        spinnerVegitable.getSelectedItem().toString();
+//      }
+//    });
+    spinnerMeat.getSelectedItem();
+
+    return view;
 
   }
 }
