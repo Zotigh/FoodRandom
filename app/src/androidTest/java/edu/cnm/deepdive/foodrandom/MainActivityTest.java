3657228c8b4a25e2cc60f496bf7827c0d5225d14
Chris <chris@speedycomputing.com>
2019-03-19 16:17:10 -0600

    A simple UI test

---
+++ app/src/androidTest/java/edu/cnm/deepdive/foodrandom/MainActivityTest.java
@@ -0,0 +1,36 @@
+package edu.cnm.deepdive.foodrandom;
+
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+
+@RunWith(AndroidJUnit4.class)
+public class MainActivityTest {
+
+    @Rule
+    public ActivityTestRule<MainActivity> activityRule
+            = new ActivityTestRule<>(MainActivity.class);
+
+
+    @Test
+    public void navigateToRandomize(){
+        //Click on Navigation
+        onView(withId(R.id.navigation_recipes))
+                .perform(click());
+
+        //Make sure fragment is loaded
+        onView(withId(R.id.saved_recipes_layout))
+                .check(matches(isDisplayed()));
+
+    }
+
+}
