4f77d957872d4472a540452fad39af04f5dd42de
Lance Zotigh <lancezotighlz@gmail.com>
2019-03-27 14:29:31 -0600

    little change

--- app/src/main/java/edu/cnm/deepdive/foodrandom/model/dao/AccessDao.java
+++ app/src/main/java/edu/cnm/deepdive/foodrandom/model/dao/AccessDao.java
@@ -13,6 +13,7 @@
  *  See the License for the specific language governing permissions and
  *  limitations under the License.
  */
+
 package edu.cnm.deepdive.foodrandom.model.dao;
 
 import android.arch.persistence.room.Dao;
